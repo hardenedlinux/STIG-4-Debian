612dd32336c7efb79ba9eb3e961caf2cc0e77084
samson <sccxboy@gmail.com>
2017-06-30 03:41:05 +0800

    Optimize the code.

--- scripts/check-deny-and-locktime.sh
+++ scripts/check-deny-and-locktime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-check_deny=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally deny ge 3`
-check_locktime=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally lock_time ge 900`
+CHECK_DENY=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally deny ge 3`
+CHECK_LOCKTIME=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally lock_time ge 900`
 
-if [ "${check_deny}"!="0" -o "${check_locktime}"!="0" ];then
+if [ "${CHECK_DENY}"!="0" -o "${CHECK_LOCKTIME}"!="0" ];then
 	exit 1
 fi

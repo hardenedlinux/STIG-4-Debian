e3d6805acf7c876c2283ca6f2426acb90de51f14
samson <sccxboy@gmail.com>
2017-07-03 12:11:18 +0800

    Fix Issue #10: The even_deny_root parameter in the pam_tally module is incorrect.

--- scripts/check-deny-and-locktime.sh
+++ scripts/check-deny-and-locktime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CHECK_DENY=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally deny ge 3`
-CHECK_LOCKTIME=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally lock_time ge 900`
+CHECK_DENY=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally2 deny ge 3`
+CHECK_LOCKTIME=`bash ./check-password.sh /etc/pam.d/common-auth pam_tally2 lock_time ge 900`
 
 if [ "${CHECK_DENY}"!="0" -o "${CHECK_LOCKTIME}"!="0" ];then
 	exit 1

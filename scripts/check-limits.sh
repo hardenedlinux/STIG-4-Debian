b86d07151958ce44114244dfd57f499d13e93987
cement-berries <47364876+cement-berries@users.noreply.github.com>
2019-06-12 12:24:03 -0600

    the check for core-dumps was not portable across different debian-based systems way of specifying limits.conf (for example, it may be done in a limits.d subdirectory or via a kernel parameter). changed it to just use ulimit -c instead

--- scripts/check-limits.sh
+++ scripts/check-limits.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 case $1 in
         core-dumps)
-                if sed -e '/^#/d' -e '/^[ \t][ \t]*#/d' -e 's/#.*$//' -e '/^$/d' /etc/security/limits.conf | grep "hard.*core";then
-                        if [ "$(sed -e '/^#/d' -e '/^[ \t][ \t]*#/d' -e 's/#.*$//' -e '/^$/d' /etc/security/limits.conf | grep "hard.*core" | awk -F 'core' '{print $2}' | sed 's/\ *//g')" -ne 0 ];then
-                                exit 1
-                        fi
-                else
+                if [ "$(ulimit -c)" -ne 0 ];then
                         exit 1
                 fi
         ;;

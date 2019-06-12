367f6ee0ea3a798e0e79613f4f2adc06de890b96
cement-berries <47364876+cement-berries@users.noreply.github.com>
2019-06-12 12:17:27 -0600

    The scripts/check-password-fail-delay.sh check should work for both /etc/login.def and /etc/pam.d/login.
    This commit handles the presence of either file, and converts the delay units appropriately depending on which you're using.

--- scripts/check-password-fail-delay.sh
+++ scripts/check-password-fail-delay.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-VFAIL_DELAY=$1
-
 if [ -f "/etc/login.def" ];then
+        VFAIL_DELAY=$1 # seconds
 
         RESULT=$(sed -e '/^#/d' -e '/^[ \t][ \t]*#/d' -e 's/#.*$//' -e '/^$/d' /etc/login.defs | grep FAIL_DELAY)
         if [ $? -eq 0 ];then
                 if [ "$(echo $RESULT | awk '{printf $2}')" -lt "${VFAIL_DELAY}" ];then
                         exit 1
-                fi  
+                fi
         else
                 exit 1
-        fi  
+        fi
+
+elif [ -f "/etc/pam.d/login" ];then
+        VFAIL_DELAY=$(($1 * 1000000)) # microseconds
+
+        RESULT=$(sed -e '/^#/d' -e '/^[ \t][ \t]*#/d' -e 's/#.*$//' -e '/^$/d' /etc/pam.d/login | grep 'pam_faildelay.so' | grep -P -o "\bdelay\s*=\s*\d+")
+        if [ $? -eq 0 ];then
+                if [ "$(echo $RESULT | cut -d= -f2-)" -lt "${VFAIL_DELAY}" ];then
+                        exit 1
+                fi
+        else
+                exit 1
+        fi
+
 else
         exit 1
 fi
